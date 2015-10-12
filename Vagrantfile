d64a8f5296d9d6871501c56bbf7db761b1f97c62
Paul Massey <paul.massey@tenforce.com>
2015-10-12 17:13:54 +0200

    Updates for fixing the version to the 2.1.3 version of the packages.

--- Vagrantfile
+++ Vagrantfile
@@ -3,14 +3,16 @@
 
 Vagrant.configure(2) do |config|
   config.vm.box = "deimosfr/debian-wheezy"
+  # 
   config.vm.provision :shell, path: "bootstrap.sh"
-  config.vm.network "public_network"
+  # 
+  # config.vm.network "public_network"
+  #
   config.vm.provider "virtualbox" do |vb|
       vb.name = "UnifiedView2.x-wheezy"
       vb.gui = true
       vb.customize ["modifyvm", :id, "--memory", 4096]
       vb.customize ["modifyvm", :id, "--vram", 64]
-      # vb.customize ["modifyvm", :id, "--accelerate3d", "on"]
       vb.customize ["modifyvm", :id, "--natdnshostresolver1", "on"]
       vb.customize ["modifyvm", :id, "--natdnsproxy1", "on"]
       vb.customize ["modifyvm", :id, "--clipboard", "bidirectional"]
@@ -18,5 +20,4 @@ Vagrant.configure(2) do |config|
       # Customize the amount of memory on the VM:
       vb.memory = "4096"
   end
-  config.vm.provision :shell, path: "bootstrap.sh"
 end
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,19 @@
-#!/usr/bin/env bashA
+#!/usr/bin/env bash
 #################################################################
-# Install the necessary components for building and installing
-# the unifiedviews system from the github.
-#
-# This is the LATEST, 2.0.1 version (not a specific branch).
-#
+# Install the necessary components for unifiedviews (packages used)
+
+#################################################################
+# This is the LATEST which has been tested (not always latest).
+VERSION=2.1.3
+
+#################################################################
 # Standard System Updates.
 apt-get install -y xinit xterm iceweasel gnome-terminal gnome-shell
 apt-get install -y dkms virtualbox-guest-dkms virtualbox-guest-x11
 apt-get install -y gdm3 apache2 libapache2-mod-auth-cas debconf-utils dpkg-dev build-essential quilt gdebi
 dpkg-reconfigure gdm3
 
+#################################################################
 # Now start to setup for building unified views, etc.
 apt-get install -y openjdk-7-jre openjdk-7-jdk
 apt-get install -y tomcat7 git maven bash emacs nano vim
@@ -20,12 +23,12 @@ echo "deb http://packages.comsode.eu/debian wheezy main" > /etc/apt/sources.list
 wget -O - http://packages.comsode.eu/key/odn.gpg.key | apt-key add -
 apt-get update -y --force-yes
 
-###############################################################
+#################################################################
 # http://www.oracle.com/technetwork/database/features/jdbc/default-2280470.html
 # should be in this directory
 mvn install:install-file -Dfile=/vagrant/ojdbc7.jar -DgroupId=com.oracle -DartifactId=ojdbc7 -Dversion=12.1.0.2.0 -Dpackaging=jar
 
-###############################################################
+#################################################################
 # Set the default values for the debconf questions
 #
 echo "mysql-server-5.5 mysql-server/root_password_again password root" | debconf-set-selections
@@ -57,16 +60,20 @@ echo "unifiedviews-backend-mysql      backend/mysql_root password root"| debconf
 echo "unifiedviews-backend-mysql      backend/mysql_db_password password root"| debconf-set-selections
 echo "unifiedviews-backend-mysql      backend/mysql_db_user string root"| debconf-set-selections
 
-##############################################################
+################################################################
 # DB installations, etc which have customised values.
 
 apt-get install -y virtuoso-opensource mysql-server iceweasel
 apt-get update -y --force-yes
 
-###############################################################
+################################################################
 # Unified views pulling, packaging and hopefully the installation.
-apt-get -y install unifiedviews-mysql
-apt-get -y install unifiedviews-plugins
+apt-get -y --force-yes install unifiedviews-mysql=${VERSION} \
+	unifiedviews-backend-shared=${VERSION} unifiedviews-backend-mysql=${VERSION} \
+	unifiedviews-backend=${VERSION} unifiedviews-webapp-shared=${VERSION} \
+	unifiedviews-webapp-mysql=${VERSION} unifiedviews-webapp=${VERSION} \
+	unifiedviews-plugins=${VERSION}
+apt-mark hold unifiedviews-mysql       # WILL NOT ALLOW UPDATING (at present)
 
 # Change the env (language changed to english :-))
 sed -iBAC -e 's/sk/en/g' /etc/unifiedviews/*.properties
@@ -74,15 +81,15 @@ sed -iBAC -e 's/sk/en/g' /etc/unifiedviews/*.properties
 bash /usr/share/unifiedviews/dist/plugins/deploy-dpus.sh
 
 # Make sure nothing missing ...
-apt-get -f -y install 
+apt-get -f -y install
 
-###############################################################
+################################################################
 # Setup other services
 # update-rc.d unifiedviews-backend defaults
 # Allows login without password
 echo "vagrant ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/vagrant
 
-###############################################################
+################################################################
 # Change the default homepage
 echo "user_pref(\"browser.startup.homepage\", \"http://localhost:28080/unifiedviews\");" >> /etc/iceweasel/pref/iceweasel.js
 echo "_user_pref(\"browser.startup.homepage\", \"http://localhost:28080/unifiedviews\");" >> /etc/iceweasel/profile/prefs.js
@@ -91,8 +98,8 @@ echo "_user_pref(\"browser.startup.homepage\", \"http://localhost:28080/unifiedv
 ( cd /vagrant/Plugin-DevEnv ; mvn install )
 ( cd /vagrant ; git clone https://github.com/tenforce/unifiedviews-dpus.git )
 
-###############################################################
+################################################################
 apt-get autoclean
 echo "****** done with bootstrap"
-###############################################################
+################################################################
 
